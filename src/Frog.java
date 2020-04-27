215d7de7e2846918c259be7962af5cb8b13d9d79
Jude <91631@farnborough.ac.uk>
2020-04-27 11:17:58 +0100

    Added objects frog1 and frog2

--- src/Frog.java
+++ src/Frog.java
@@ -1,10 +1,16 @@
+import java.awt.*;
+
 public class Frog {
     private String colour;
     private int position;
+    private String name;
+    private int age;
 
-    public Frog(String colour, int position){
+    public Frog(String colour, int position, String name, int age){
         this.colour = colour;
         this.position = position;
+        this.name = name;
+        this.age = age;
     }
 
     public String getColour(){
@@ -15,11 +21,27 @@ public class Frog {
         return position;
     }
 
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
     public void setColour(String colour){
         this.colour = colour;
     }
 
     public void setPosition(int position){
         this.position = position;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args){
-        
+        Frog frog1 = new Frog("green", 10, "Bob", 12);
+        frog1.setColour("red");
+        System.out.println(frog1.getColour());
+        frog1.setName("Steve");
+        System.out.println(frog1.getName());
+
+        Frog frog2 = new Frog("red", 6, "Jeff", 3);
+        frog2.setPosition(3);
+        System.out.println("Position: " + frog2.getPosition());
+        frog2.setAge(4);
+        System.out.println("Age: " + frog2.getAge());
+        frog2.setColour("Pink");
+        System.out.println("Colour: " + frog2.getColour());
     }
 }
